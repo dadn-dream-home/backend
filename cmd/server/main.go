2573dea71eb3a4c63368428b68fb0ccfd448c72f
Khang Nguyen Duy <khang.nguyenduycse@hcmut.edu.vn>
2023-05-01 13:44:33 +0700

    mqtt adaptor

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,42 +1,26 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"net"
+import "sync"
 
-	mqtt "github.com/eclipse/paho.mqtt.golang"
-)
-
-type Backend struct {
-	mqtt.Client
-}
-
-func newBackend() Backend {
-	opts := mqtt.NewClientOptions()
-	opts.AddBroker("tcp://localhost:1883")
-	client := mqtt.NewClient(opts)
-
-	if t := client.Connect(); t.Wait() && t.Error() != nil {
-		panic(t.Error())
-	}
-
-	log.Printf("connected to broker\n")
-
-	return Backend{client}
-}
+var wg sync.WaitGroup
 
 func main() {
-	backend := newBackend()
-	s := NewBackendGrpcService(backend.Client)
+	// backend := newBackend()
+	// s := NewBackendGrpcService(backend.Client)
 
-	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", 8080))
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
-	}
+	// lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", 8080))
+	// if err != nil {
+	// 	log.Fatalf("failed to listen: %v", err)
+	// }
 
-	log.Printf("server listening at %v\n", lis.Addr())
-	if err := s.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
-	}
+	// log.Printf("server listening at %v\n", lis.Addr())
+	// if err := s.Serve(lis); err != nil {
+	// 	log.Fatalf("failed to serve: %v", err)
+	// }
+
+	mqtt := NewMQTTClient()
+	tx, _ := mqtt.Subscribe("test")
+	close(tx)
+
+	wg.Wait()
 }
---
+++ cmd/server/mqtt.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"log"
+	"sync"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+)
+
+type MQTTClient struct {
+	sync.Mutex
+	oldClient     mqtt.Client
+	topicChannels map[string]bool
+}
+
+func NewMQTTClient() MQTTClient {
+	opts := mqtt.NewClientOptions()
+	opts.AddBroker("tcp://localhost:1883")
+	client := mqtt.NewClient(opts)
+
+	if t := client.Connect(); t.Wait() && t.Error() != nil {
+		panic(t.Error())
+	}
+
+	return MQTTClient{
+		oldClient:     client,
+		topicChannels: make(map[string]bool),
+	}
+}
+
+// Establishes a bidirectional channel to the topic
+func (c *MQTTClient) Subscribe(topic string) (tx chan<- []byte, rx <-chan []byte) {
+	c.Lock()
+	if _, ok := c.topicChannels[topic]; ok {
+		panic("already subscribed to topic " + topic)
+	}
+	c.topicChannels[topic] = true
+	c.Unlock()
+
+	tch := make(chan []byte)
+	rch := make(chan []byte)
+
+	// QOS = 2 because we want to ensure that the message is delivered exactly once
+	// for simplicity, not for reliability
+	var qos byte = 2
+	if t := c.oldClient.Subscribe(topic, qos, func(_ mqtt.Client, m mqtt.Message) {
+		rch <- m.Payload()
+	}); t.Wait() && t.Error() != nil {
+		panic(t.Error())
+	}
+
+	wg.Add(1)
+	go func() {
+		c.listen(topic, tch)
+		// tx is closed
+		delete(c.topicChannels, topic)
+		log.Printf("unsubscribed from topic %v\n", topic)
+		close(rch)
+		wg.Done()
+	}()
+
+	log.Printf("subscribed to topic %v\n", topic)
+
+	return tch, rch
+}
+
+func (c *MQTTClient) listen(topic string, tx <-chan []byte) {
+	for payload := range tx {
+		// for simplicity, not for reliability
+		var qos byte = 2
+		var retained bool = true
+		if t := c.oldClient.Publish(topic, qos, retained, payload); t.Wait() && t.Error() != nil {
+			panic(t.Error())
+		}
+
+		log.Printf("published topic %v: %s\n", topic, string(payload))
+	}
+}
